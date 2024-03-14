8a70f10e1c301431ec9743d6d45d45ad88038308
KishanCont <kishan.kumar@contrivers.com>
2024-03-14 16:15:00 +0530

    Add console

--- app/api/route.js
+++ app/api/route.js
@@ -2,6 +2,7 @@ import { AUTHORIZATION_URL } from "@/constants";
 
 export const GET = (req, res) => {
   try {
+    console.log(AUTHORIZATION_URL);
     return Response.redirect(AUTHORIZATION_URL);
   } catch (error) {
     console.log(error);
