90f3fd2c14d28dff512f6885cbd8bcd82ca2ed91
KishanCont <kishan.kumar@contrivers.com>
2024-03-14 13:26:55 +0530

    Fix URL Redirect

--- app/api/route.js
+++ app/api/route.js
@@ -2,5 +2,5 @@ import { AUTHORIZATION_URL } from "@/constants";
 
 export const GET = (request, response) => {
   // Redirect the user to the HubSpot OAuth 2.0 authorization URL
-  return Response.redirect(AUTHORIZATION_URL);
+  return Response.redirect(new URL(AUTHORIZATION_URL));
 };
