22854d57c2256fc456088c5559446aed805a5167
KishanCont <kishan.kumar@contrivers.com>
2024-03-14 16:22:59 +0530

    Remove Console

--- app/api/route.js
+++ app/api/route.js
@@ -2,8 +2,9 @@ import { AUTHORIZATION_URL } from "@/constants";
 
 export const GET = (req, res) => {
   try {
-    console.log(AUTHORIZATION_URL);
-    return Response.redirect(AUTHORIZATION_URL);
+    return Response.json({ url: AUTHORIZATION_URL }).redirected(
+      AUTHORIZATION_URL
+    );
   } catch (error) {
     console.log(error);
   }
