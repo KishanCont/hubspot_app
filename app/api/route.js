d894f1c830d00275368259e813ccc7c4eceae758
KishanCont <kishan.kumar@contrivers.com>
2024-03-14 16:28:54 +0530

    Send response as redirected uri

--- app/api/route.js
+++ app/api/route.js
@@ -2,9 +2,7 @@ import { AUTHORIZATION_URL } from "@/constants";
 
 export const GET = (req, res) => {
   try {
-    return Response.json({ url: AUTHORIZATION_URL }).redirected(
-      AUTHORIZATION_URL
-    );
+    return Response.json({ url: AUTHORIZATION_URL });
   } catch (error) {
     console.log(error);
   }
