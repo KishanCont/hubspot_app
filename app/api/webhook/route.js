b258436c533c0c15c1f3ce1796044b6231f57b56
KishanCont <kishan.kumar@contrivers.com>
2024-03-21 22:47:18 +0530

    Final Commit

--- app/api/webhook/route.js
+++ app/api/webhook/route.js
@@ -5,15 +5,15 @@ export const POST = async (req, res) => {
   try {
     const dbClient = await createMongoConnection();
     const data = await req.json();
-    // const { objectId, portalId, subscriptionType } = data[0];
-    // const dbName = `Account_${portalId}`;
-    // const db = dbClient.db(dbName);
+    const { objectId, portalId, subscriptionType } = data[0];
+    const dbName = `Account_${portalId}`;
+    const db = dbClient.db(dbName);
 
-    // if (subscriptionType === "product.creation") {
-    //   await createProductCollection(db, objectId, Number(portalId));
-    // } else if (subscriptionType === "product.deletion") {
-    //   await dropCollection(db, objectId, Number(portalId));
-    // }
+    if (subscriptionType === "product.creation") {
+      await createProductCollection(db, objectId, Number(portalId));
+    } else if (subscriptionType === "product.deletion") {
+      await dropCollection(db, objectId, Number(portalId));
+    }
 
     dbClient.close();
     return Response.json({ data });
